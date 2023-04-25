6a7346022c690fc45393afe333b73a0dc93d124d
shaohanzhong <shaomenghao@hotmail.com>
2023-04-25 22:38:16 +0800

    update

---
+++ SloganMaker/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.KernelExtensions;
+using Microsoft.SemanticKernel.Orchestration;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("please input your OpenAI key ...");
+        var key = Console.ReadLine();
+
+        var sk = Kernel.Builder.Build();
+        sk.Config.AddOpenAITextCompletionService(
+            "OpenAI_davinci", "text-davinci-003",
+            key);
+
+        var skill = sk.ImportSemanticSkillFromDirectory("MySkillsDirectory", "TestSkill");
+        var context = new ContextVariables();
+        context.Set("input", "Basketweaving Service");
+
+        var result = await sk.RunAsync(context, skill["SloganMaker"]);
+        await Console.Out.WriteLineAsync(result.Result);
+    }
+}
+
