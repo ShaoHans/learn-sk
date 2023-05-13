8e198e65bb13ccbb2eb67ea65b4e416b357fadac
shaohanzhong <shaomenghao@hotmail.com>
2023-05-13 17:23:54 +0800

    结构化输出

---
+++ PromptEngineering/P02_结构化输出.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+
+namespace PromptEngineering;
+
+internal class P02_结构化输出 : IPromptRunner
+{
+    public async Task RunAsync()
+    {
+        var config = new OpenAIConfig
+        {
+            Template = "推荐.Net框架在欧美最受欢迎的三本书，包含书的英文名、中文名、作者、页数，按照json格式输出，json的key为：name、zhName、author、pages"
+        };
+
+        var response = await OpenAI.GetTextCompletionAsync(config);
+        Console.WriteLine(config.Template);
+        Console.WriteLine(config.Input);
+        Console.WriteLine(response);
+    }
+}
--- PromptEngineering/Program.cs
+++ PromptEngineering/Program.cs
@@ -4,7 +4,28 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        var runner = new P01_提示分隔符();
-        await runner.RunAsync();
+        IPromptRunner runner = null;
+        Console.WriteLine("请输入案例索引号：");
+        var index = Console.ReadLine();
+        switch (index)
+        {
+            case "1":
+                runner = new P01_提示分隔符();
+                break;
+            case "2":
+                runner = new P02_结构化输出();
+                break;
+            default:
+                break;
+        }
+
+        if (runner != null)
+        {
+            await runner.RunAsync();
+        }
+        else
+        {
+            Console.WriteLine("无效的索引号");
+        }
     }
 }
