57bf4df6616b11f6594fedc885b1d73cf0ad3448
shaohanzhong <shaomenghao@hotmail.com>
2023-05-22 22:47:18 +0800

    P16_数据格式转换

---
+++ PromptEngineering/P16_数据格式转换.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+
+namespace PromptEngineering;
+
+internal class P16_数据格式转换 : IPromptRunner
+{
+    public async Task RunAsync()
+    {
+        var data = new[]
+        {
+            new{ Name="张三",Email="zhangsan@qq.com"},
+            new{ Name="李四",Email="lisi@qq.com"},
+            new{ Name="王五",Email="wangwu@qq.com"}
+        };
+
+        var config = new OpenAIConfig
+        {
+            Template = @"请将【】标签内的json格式数据转成html。
+【{{$Input}}】",
+
+            Input = System.Text.Json.JsonSerializer.Serialize(data)
+        };
+
+        var response = await OpenAI.GetTextCompletionAsync(config);
+        OpenAI.Output(config, response);
+    }
+}
