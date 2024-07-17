586d2f276e6009839124bfe0c3742b0ff88c888b
shaohanzhong <shaomenghao@hotmail.com>
2024-07-17 22:56:46 +0800

    update

---
+++ 01QuickStart/QuickStart/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion("test01", "https://shaohanzhongopenaidemo.openai.azure.com/", "");
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+Kernel kernel = builder.Build();
+kernel.Plugins.AddFromType<LightsPlugin>();
+var settings = new OpenAIPromptExecutionSettings()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+
+var result = await chatCompletionService.GetChatMessageContentAsync(history, settings, kernel);
