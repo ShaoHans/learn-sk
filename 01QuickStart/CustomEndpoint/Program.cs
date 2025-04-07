15edc68735f32af65c352de055f55d0f5d114734
ShaoHans <shaomenghao@hotmail.com>
2025-04-07 21:12:47 +0800

    custom endpoint

---
+++ 01QuickStart/CustomEndpoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+#pragma warning disable SKEXP0010 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
+Console.WriteLine("请输入API密钥：");
+var apiKey = Console.ReadLine();
+builder.AddOpenAIChatCompletion(
+    modelId: "glm-4-flash",
+    apiKey: apiKey,
+    endpoint: new Uri("https://open.bigmodel.cn/api/paas/v4/")
+);
+
+var kernel = builder.Build();
+var response = await kernel.InvokePromptAsync("你是谁？");
+Console.WriteLine(response.ToString());
+
+#pragma warning restore SKEXP0010 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
---
+++ 01QuickStart/CustomOpenAIClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ClientModel;
+using Microsoft.SemanticKernel;
+using OpenAI;
+
+Console.WriteLine("请输入apikey：");
+var apikey = Console.ReadLine();
+
+var builder = Kernel.CreateBuilder();
+var options = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://open.bigmodel.cn/api/paas/v4/")
+};
+
+var openAIClient = new OpenAIClient(new ApiKeyCredential(apikey!), options);
+builder.AddOpenAIChatCompletion(modelId: "glm-4-flash", openAIClient: openAIClient);
+var kernel = builder.Build();
+var response = await kernel.InvokePromptAsync("你是谁？");
+Console.WriteLine(response.ToString());
